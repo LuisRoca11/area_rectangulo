b2fdc72d72e01ca5032502c88744b9428dabca7f
Luis <lroca1102@gmail.com>
2025-09-04 23:02:34 -0300

    area de rectangulo

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿//Calcular el área de un rectángulo (base*altura)
+using System;
+
+namespace area_rectangulo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ingrese la base: ");
+            int _base = int.Parse(Console.ReadLine());
+
+            Console.Write("Ingrese la altura: ");
+            int altura = int.Parse(Console.ReadLine());
+
+            int area = altura * _base;
+            Console.WriteLine($"El area del rectangulo es: {area}");
+
+            Console.ReadKey();
+        }
+    }
+}
